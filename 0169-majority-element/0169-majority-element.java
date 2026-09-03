5a1279c06dd16004a0a23785c7731fb6543ac428
dpoojari671 <dpoojari671@gmail.com>
2026-09-03 23:30:29 +0530

    Time: 2 ms (70.71%), Space: 55.8 MB (17.94%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int freq=0; int ans=0;
+        for(int i=0; i<=n-1; i++)
+        {
+            if(freq==0)
+            {
+                ans=nums[i];
+            }
+            if(ans==nums[i])
+            {
+                freq++;
+            }
+            else
+             {
+                freq--;
+             }
+        }
+        return ans;
+    }
+}
